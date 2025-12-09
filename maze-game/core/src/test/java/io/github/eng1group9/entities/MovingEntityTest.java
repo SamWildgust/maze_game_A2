75b2f9e93c924e8bc24d6b95258faa16cdc6aa26
rosiehg <xsk506@york.ac.uk>
2025-12-09 13:19:55 +0000

    MovingEntityTest class base

---
+++ maze-game/core/src/test/java/io/github/eng1group9/entities/MovingEntityTest.java
@@ -0,0 +1,22 @@
+package io.github.eng1group9.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.graphics.Texture;
+
+import io.github.eng1group9.Main;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyFloat;
+import static org.mockito.Mockito.*;
+
+/**
+ * Unit tests for the MovingEntity class.
+ */
